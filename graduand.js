e5f7cc768145daaa5f6cfefe44973b53d00d1491
unknown <barasabrandon@gmail.com>
2024-12-17 18:17:23 +0300

    initial

---
+++ graduand.js
@@ -0,0 +1,44 @@
+function displayGraduant() {
+  const graduantsArray = ['John', 'Jane', 'James', 'Jill'];
+  graduantsArray.forEach((granduand, index) => {
+    console.log(`Congratulations ${granduand} on your graduation! ${index}`);
+  });
+
+  const graduantsObject = { name: 'Sharon Murithi', course: 'Computer Science ', year: 2027 };
+  console.log(
+    'Congratulations ' + graduantsObject.name + 'on your graduation in',
+    graduantsObject.course + graduantsObject.year
+  );
+
+  const graduadionDetails = [
+    { name: 'Sharon Murithi', course: 'Computer Science ', year: 2027, units: ['COM 101', 'COM 102', 'COM 103'] },
+    { name: 'John', course: 'Computer Science ', year: 2027, units: ['COM 101', 'COM 102', 'COM 103'] },
+    { name: 'Jane', course: 'Computer Science ', year: 2027, units: ['COM 101', 'COM 102', 'COM 103'] },
+    { name: 'James', course: 'Computer Science ', year: 2027, units: ['COM 101', 'COM 102', 'COM 103'] },
+    { name: 'Jill', course: 'Computer Science ', year: 2027, units: ['COM 101', 'COM 102', 'COM 103'] },
+  ];
+
+  graduadionDetails.forEach((item, index) => {
+    console.log(item.name);
+    item.units.forEach((unit, index) => {
+      console.log(unit);
+    });
+  });
+
+  const getGraduandContainer = document.getElementById('graduands');
+  if (getGraduandContainer) {
+    console.log('Container found');
+    const graduandElement = document.createElement('div');
+    graduandElement.classList.add('graduand');
+
+    graduandElement.innerHTML = `   <div>Name: Iren nasieku</div>
+        <div>School: Biological and Physical Sciences</div>
+        <div>Course: Computer Science</div>
+        <div>Reg No: COM/2541/23</div>`;
+
+    getGraduandContainer.appendChild(graduandElement);
+  } else {
+    console.log('Container not found');
+  }
+}
+displayGraduant();
